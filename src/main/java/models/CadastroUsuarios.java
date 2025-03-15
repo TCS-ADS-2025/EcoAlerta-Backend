da44d9a0ec7d8292208426494bea3937e3950d2c
RenanZanette <141188417+RenanZanette@users.noreply.github.com>
2025-03-15 16:57:24 -0300

    Create CadastroUsuarios.java
    feat: criado model  cadastro usuarios

---
+++ src/main/java/models/CadastroUsuarios.java
@@ -0,0 +1,32 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class CadastroUsuarios extends EntityID {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column
+    private int id;
+    @Column
+    private String nomeCompleto;
+    @Column
+    private String email;
+    @Column
+    private String senha;
+
+    @Override
+    public String toString() {
+        return "CadastroUsuarios{" +
+                "id=" + id +
+                ", nomeCompleto='" + nomeCompleto + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
+}
