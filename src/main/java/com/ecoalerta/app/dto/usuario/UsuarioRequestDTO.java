a5d498dac3fdb572f51317f66e1addb4833a8e4e
Marcus Almeida <marcusvinicius20.almeida@gmail.com>
2025-03-18 02:43:06 -0300

    feat: criado modelo de dtos para a continuidade do projeto

--- src/main/java/com/ecoalerta/app/dto/usuario/UsuarioRequestDTO.java
+++ src/main/java/com/ecoalerta/app/dto/usuario/UsuarioRequestDTO.java
@@ -1,10 +1,22 @@
 package com.ecoalerta.app.dto.usuario;
 
+import com.ecoalerta.app.models.Endereco;
+import com.ecoalerta.app.models.Usuario;
 
 public record UsuarioRequestDTO(
 
     String nomeCompleto,
+    Endereco endereco,
     String email,
     String senha
     ) {
+
+    public Usuario toEntity() {
+        return new Usuario(
+                this.nomeCompleto,
+                this.endereco,
+                this.email,
+                this.senha
+        );
+    }
 }
