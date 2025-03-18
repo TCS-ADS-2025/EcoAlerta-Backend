fdafc6afbbf82a5822042f6c0e4015e6e6e346b7
Marcus Almeida <marcusvinicius20.almeida@gmail.com>
2025-03-18 02:48:14 -0300

    feat: criado modelo de controller para a continuidade do projeto

---
+++ src/main/java/com/ecoalerta/app/controllers/UsuarioController.java
@@ -0,0 +1,31 @@
+package com.ecoalerta.app.controllers;
+
+import com.ecoalerta.app.dto.usuario.UsuarioRequestDTO;
+import com.ecoalerta.app.dto.usuario.UsuarioResponseDTO;
+import com.ecoalerta.app.services.UsuarioService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService service;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<String> criar(@RequestBody @Valid UsuarioRequestDTO request) {
+        UsuarioResponseDTO response = service.criar(request);
+        return ResponseEntity.ok("Usuário cadastrado com sucesso!");
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<UsuarioResponseDTO>> listar() {
+        List<UsuarioResponseDTO> usuarios = service.listar();
+        return ResponseEntity.ok(usuarios);
+    }
+}
