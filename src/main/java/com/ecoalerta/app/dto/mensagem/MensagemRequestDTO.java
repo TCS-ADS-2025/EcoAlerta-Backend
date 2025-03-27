34b4af4f69ffc69d5e3512036c1da487613bd3ba
Marcus Almeida <marcusvinicius20.almeida@gmail.com>
2025-03-27 12:22:17 -0300

    fix: correções feitas nos dtos e service de Mensagem e enums substituidos por Boolean

--- src/main/java/com/ecoalerta/app/dto/mensagem/MensagemRequestDTO.java
+++ src/main/java/com/ecoalerta/app/dto/mensagem/MensagemRequestDTO.java
@@ -1,33 +1,24 @@
 package com.ecoalerta.app.dto.mensagem;
 
-import com.ecoalerta.app.models.Mensagem;
-import com.ecoalerta.app.models.Usuario;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.UUID;
+
 public record MensagemRequestDTO (
 
-    @NotBlank(message = "Título é um campo obrigatório!")
-    @Size(max = 50)
-    String titulo,
+        @NotBlank(message = "Título é um campo obrigatório!")
+        @Size(max = 50)
+        String titulo,
 
-    @NotBlank(message = "Destinatário é um campo obrigatório!")
-    @Size(max = 100)
-    String destinatario,
+        @NotBlank(message = "Destinatário é um campo obrigatório!")
+        @Size(max = 100)
+        String destinatario,
 
-    @NotBlank(message = "Mensagem é um campo obrigatório!")
-    @Size(max = 250)
-    String mensagem,
+        @NotBlank(message = "Mensagem é um campo obrigatório!")
+        @Size(max = 250)
+        String mensagem,
 
-    Usuario usuario
+        UUID usuarioId
 ) {
-
-    public Mensagem toEntity(){
-        return new Mensagem(
-                this.titulo,
-                this.destinatario,
-                this.mensagem,
-                this.usuario
-        );
-    }
 }
--- src/main/java/com/ecoalerta/app/services/MensagemService.java
+++ src/main/java/com/ecoalerta/app/services/MensagemService.java
@@ -3,7 +3,10 @@ package com.ecoalerta.app.services;
 import com.ecoalerta.app.dto.mensagem.MensagemRequestDTO;
 import com.ecoalerta.app.dto.mensagem.MensagemResponseDTO;
 import com.ecoalerta.app.models.Mensagem;
+import com.ecoalerta.app.models.Usuario;
 import com.ecoalerta.app.repositories.MensagemRepository;
+import com.ecoalerta.app.repositories.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,18 +16,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MensagemService {
 
-    private final MensagemRepository repository;
+    private final MensagemRepository mensagemRepository;
+    private final UsuarioRepository usuarioRepository;
 
-    public MensagemResponseDTO criar(MensagemRequestDTO request){
-        Mensagem mensagem = request.toEntity();
+    public MensagemResponseDTO criar(MensagemRequestDTO request) {
+        Usuario usuario = usuarioRepository.findById(request.usuarioId())
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
 
-        Mensagem salvo = repository.save(mensagem);
+        Mensagem mensagem = new Mensagem();
+        mensagem.setTitulo(request.titulo());
+        mensagem.setDestinatario(request.destinatario());
+        mensagem.setMensagem(request.mensagem());
+        mensagem.setUsuario(usuario);
+
+        Mensagem salvo = mensagemRepository.save(mensagem);
 
         return MensagemResponseDTO.fromEntity(salvo);
     }
 
     public List<MensagemResponseDTO> listarTodos(){
-        return repository.findAll()
+        return mensagemRepository.findAll()
                 .stream()
                 .map(MensagemResponseDTO::fromEntity)
                 .toList();
