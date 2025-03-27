fe6015d5772cb3db3036fbb3fd57bb5a565bb315
Marcus Almeida <marcusvinicius20.almeida@gmail.com>
2025-03-27 12:26:01 -0300

    fix: correcoes de espacamento e validacoes de campos

--- src/main/java/com/ecoalerta/app/models/Usuario.java
+++ src/main/java/com/ecoalerta/app/models/Usuario.java
@@ -22,6 +22,15 @@ import java.util.List;
 @AllArgsConstructor
 public class Usuario extends EntityID {
 
+    @Column(name = "nome_completo", nullable = false)
+    private String nomeCompleto;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "endereco_id", referencedColumnName = "id")
     @JsonIgnore
@@ -30,21 +39,6 @@ public class Usuario extends EntityID {
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private List<Mensagem> mensagens = new ArrayList<>();
-
-    @NotBlank(message = "Nome é um campo obrigatório!")
-    @Size(min = 3, message = "o nome deve conter no mínimo 3 caractéres!")
-    @Column(name = "nome_completo", nullable = false)
-    private String nomeCompleto;
-
-    @NotBlank(message = "E-mail é um campo obrigatório!")
-    @Email
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @NotBlank(message = "Senha é um campo obrigatório!")
-    @Size(min = 8, message = "A senha deve conter no mínimo 8 caractéres!")
-    @Column(nullable = false)
-    private String senha;
 
     public Usuario(String nomeCompleto, Endereco endereco, String email, String senha) {
         super();
--- src/main/java/com/ecoalerta/app/services/UsuarioService.java
+++ src/main/java/com/ecoalerta/app/services/UsuarioService.java
@@ -1,8 +1,6 @@
 package com.ecoalerta.app.services;
 
-import com.ecoalerta.app.dto.usuario.UsuarioRequestDTO;
 import com.ecoalerta.app.dto.usuario.UsuarioResponseDTO;
-import com.ecoalerta.app.models.Usuario;
 import com.ecoalerta.app.repositories.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,14 +12,6 @@ import java.util.List;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
-
-    public UsuarioResponseDTO criar(UsuarioRequestDTO request) {
-        Usuario usuario = request.toEntity();
-
-        Usuario salvo = usuarioRepository.save(usuario);
-
-        return UsuarioResponseDTO.fromEntity(salvo);
-    }
 
     public List<UsuarioResponseDTO> listar() {
         return usuarioRepository.findAll()
