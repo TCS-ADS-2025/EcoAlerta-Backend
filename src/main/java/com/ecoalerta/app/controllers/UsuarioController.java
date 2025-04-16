915802f546e3e4da473c4ff6cdf0f75c081b4442
RenanZanette <renan_zanette2012@hotmail.com>
2025-04-16 20:23:00 -0300

    Update UsuarioService
    feat: update usuario service

--- src/main/java/com/ecoalerta/app/controllers/UsuarioController.java
+++ src/main/java/com/ecoalerta/app/controllers/UsuarioController.java
@@ -2,6 +2,7 @@ package com.ecoalerta.app.controllers;
 
 import com.ecoalerta.app.dto.usuario.UsuarioRequestDTO;
 import com.ecoalerta.app.dto.usuario.UsuarioResponseDTO;
+import com.ecoalerta.app.models.Usuario;
 import com.ecoalerta.app.services.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,7 @@ public class UsuarioController {
 
     @PostMapping("/cadastrar")
     public ResponseEntity<String> criar(@RequestBody @Valid UsuarioRequestDTO request) {
-        UsuarioResponseDTO response = service.criar(request);
+        Usuario response = service.criar(request);
         return ResponseEntity.ok("Usuário cadastrado com sucesso!");
     }
 
