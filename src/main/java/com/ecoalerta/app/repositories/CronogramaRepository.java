483f62036a9c410909a7a5abfa848e4508a7c7f1
RenanZanette <renan_zanette2012@hotmail.com>
2025-04-16 20:45:45 -0300

    Create Notificacao Service
    feat: criado notificacao service

--- src/main/java/com/ecoalerta/app/repositories/CronogramaRepository.java
+++ src/main/java/com/ecoalerta/app/repositories/CronogramaRepository.java
@@ -1,11 +1,15 @@
 package com.ecoalerta.app.repositories;
 
 import com.ecoalerta.app.models.Cronograma;
+import com.ecoalerta.app.models.enums.DiaSemana;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CronogramaRepository extends JpaRepository<Cronograma, UUID> {
+
+    List<Cronograma> findByDiaSemana(DiaSemana diaSemana);
 }
